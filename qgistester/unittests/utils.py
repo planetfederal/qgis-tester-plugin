756962efe5ad8056cc6531d4c3790541fad28e69
Luigi Pirelli <luipir@gmail.com>
2016-04-13 15:50:28 +0200

    added qgistester module in pythonpath

--- qgistester/unittests/utils.py
+++ qgistester/unittests/utils.py
@@ -4,12 +4,17 @@
 # (c) 2016 Boundless, http://boundlessgeo.com
 # This code is licensed under the GPL 2.0 license.
 #
+import os
 import sys
 import logging
-
 import sip
-for api in ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]:
+for api in ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl",
+            "QVariant"]:
     sip.setapi(api, 2)
+# add current module to pythonpath
+path = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.pardir,
+                    os.path.pardir)
+sys.path.insert(1, path)
 
 
 LOGGER = logging.getLogger('QGIS')
