ae22504746a21c7636614a1844da256f940906b3
Luigi Pirelli <luipir@gmail.com>
2016-04-14 11:12:58 +0200

    changed tabs to 4spaces

--- qgistester/tests/__init__.py
+++ qgistester/tests/__init__.py
@@ -12,31 +12,31 @@ from qgistester.test import Test, UnitTestWrapper
 
 
 def findTests():
-	_tests = []
-	prefix = __name__ + "."
-	path = __path__
-	for importer, modname, ispkg in pkgutil.iter_modules(path, prefix):
-		modtests = []
-		group = modname.split(".")[-1]
-		module = __import__(modname, fromlist="dummy")
-		if "functionalTests" in dir(module):
-			modtests.extend(module.functionalTests())
-		if "unitTests" in dir(module):
-			modtests.extend([UnitTestWrapper(unit) for unit in module.unitTests()])
-		for t in modtests:
-			t.group = group
-		_tests.extend(modtests)
-	return  _tests
+    _tests = []
+    prefix = __name__ + "."
+    path = __path__
+    for importer, modname, ispkg in pkgutil.iter_modules(path, prefix):
+        modtests = []
+        group = modname.split(".")[-1]
+        module = __import__(modname, fromlist="dummy")
+        if "functionalTests" in dir(module):
+            modtests.extend(module.functionalTests())
+        if "unitTests" in dir(module):
+            modtests.extend([UnitTestWrapper(unit) for unit in module.unitTests()])
+        for t in modtests:
+            t.group = group
+        _tests.extend(modtests)
+    return  _tests
 
 tests = findTests()
 
 def addTestModule(module, group = None):
-	modtests = []
-	group = group or module.__name__.split(".")[-1]
-	if "functionalTests" in dir(module):
-		modtests.extend(module.functionalTests())
-	if "unitTests" in dir(module):
-		modtests.extend([UnitTestWrapper(unit) for unit in module.unitTests()])
-	for t in modtests:
-		t.group = group
-	tests.extend(modtests)
+    modtests = []
+    group = group or module.__name__.split(".")[-1]
+    if "functionalTests" in dir(module):
+        modtests.extend(module.functionalTests())
+    if "unitTests" in dir(module):
+        modtests.extend([UnitTestWrapper(unit) for unit in module.unitTests()])
+    for t in modtests:
+        t.group = group
+    tests.extend(modtests)
