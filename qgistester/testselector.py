2941b6c86ec77bcde0bce7c9471fcc6ca3ee28b5
volaya <volayaf@gmail.com>
2016-06-16 10:26:51 +0200

    better handling of difference between unit and manual tests in selector

--- qgistester/testselector.py
+++ qgistester/testselector.py
@@ -35,44 +35,56 @@ class TestSelector(BASE, WIDGET):
             groupItem = QTreeWidgetItem()
             groupItem.setText(0, group)
             groupItem.setFlags(groupItem.flags() | Qt.ItemIsTristate);
+            unitItem = QTreeWidgetItem()
+            unitItem.setText(0, "Unit tests")
+            unitItem.setFlags(unitItem.flags() | Qt.ItemIsTristate);
+            manualItem = QTreeWidgetItem()
+            manualItem.setText(0, "Manual tests")
+            manualItem.setFlags(manualItem.flags() | Qt.ItemIsTristate);
             for test in groupTests:
                 testItem = QTreeWidgetItem()
                 testItem.setFlags(testItem.flags() | Qt.ItemIsUserCheckable);
                 testItem.setCheckState(0, Qt.Checked);
                 testItem.test = test
                 testItem.setText(0, test.name)
-                groupItem.addChild(testItem)
+                if isinstance(test, UnitTestWrapper):
+                    unitItem.addChild(testItem)
+                else:
+                    manualItem.addChild(testItem)
+            if manualItem.childCount():
+                groupItem.addChild(manualItem)                
+            if unitItem.childCount():
+                groupItem.addChild(unitItem)
             self.testsTree.addTopLevelItem(groupItem)
-
-        settings = QSettings('Boundless', 'qgistester')
-        showUnitTests = settings.value('showUnittests', False, bool)
-        self.showUnitTestsCheck.setChecked(showUnitTests)
-        if not showUnitTests:
-            self.filterUnitTests()
+            groupItem.setExpanded(True)
 
         self.buttonBox.accepted.connect(self.okPressed)
         self.buttonBox.rejected.connect(self.cancelPressed)
 
-        self.selectAllLabel.linkActivated.connect(lambda: self.checkTests(True))
-        self.unselectAllLabel.linkActivated.connect(lambda: self.checkTests(False))
-        self.showUnitTestsCheck.stateChanged.connect(self.filterUnitTests)
+        self.selectAllLabel.linkActivated.connect(lambda: self.checkTests(lambda t: Qt.Checked))
+        self.unselectAllLabel.linkActivated.connect(lambda: self.checkTests(lambda t: Qt.Unchecked))
+        def _onlyManual(t):
+            if isinstance(t, UnitTestWrapper):
+                return Qt.Unchecked
+            else:
+                return Qt.Checked
+        self.onlyManualLabel.linkActivated.connect(lambda: self.checkTests(_onlyManual))
+        def _onlyUnit(t):
+            if isinstance(t, UnitTestWrapper):
+                return Qt.Checked
+            else:
+                return Qt.Unchecked
+        self.onlyUnitLabel.linkActivated.connect(lambda: self.checkTests(_onlyUnit))
 
-    def filterUnitTests(self):
-        hidden = not self.showUnitTestsCheck.isChecked()
+
+    def checkTests(self, condition):
         for i in xrange(self.testsTree.topLevelItemCount()):
             item = self.testsTree.topLevelItem(i)
             for j in xrange(item.childCount()):
                 child = item.child(j)
-                if isinstance(child.test, UnitTestWrapper):
-                    child.setHidden(hidden)
-
-    def checkTests(self, b):
-        state = Qt.Checked if b else Qt.Unchecked
-        for i in xrange(self.testsTree.topLevelItemCount()):
-            item = self.testsTree.topLevelItem(i)
-            for j in xrange(item.childCount()):
-                child = item.child(j)
-                child.setCheckState(0, state)
+                for k in xrange(child.childCount()):
+                    subchild = child.child(k)
+                    child.setCheckState(0, condition(subchild.test))
 
     def cancelPressed(self):
         self.saveSettings()
@@ -83,12 +95,9 @@ class TestSelector(BASE, WIDGET):
         for i in xrange(self.testsTree.topLevelItemCount()):
             groupItem = self.testsTree.topLevelItem(i)
             for j in xrange(groupItem.childCount()):
-                testItem = groupItem.child(j)
-                if testItem.checkState(0) == Qt.Checked and not testItem.isHidden():
-                    self.tests.append(testItem.test)
-        self.saveSettings()
+                subgroupItem = groupItem.child(j)
+                for k in xrange(subgroupItem.childCount()):
+                    testItem = subgroupItem.child(k)
+                    if testItem.checkState(0) == Qt.Checked:
+                        self.tests.append(testItem.test)
         self.close()
-
-    def saveSettings(self):
-        settings = QSettings('Boundless', 'qgistester')
-        settings.setValue('showUnittests', self.showUnitTestsCheck.isChecked())
